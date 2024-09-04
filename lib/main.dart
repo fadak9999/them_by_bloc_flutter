b110f9ee4efb80ce28f95b7eb029f6f4992e7291
fadak <f9999cat.6.29kk@gmail.com>
2024-09-04 15:05:53 +0300

    main2

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:them_by_bloc_flutter/bloc/theme_bloc.dart';
-// import 'package:them_by_bloc_flutter/home.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocProvider<ThemeBloc>(
-//       create: (context) => ThemeBloc().add(GetCurrentThemeEvent()),
-//       //
-//       child: BlocBuilder<ThemeBloc, SubjectState>(
-//         builder: (context, state) {
-//           if (state is LodingThemeState) {
-//             return MaterialApp(
-//               theme: state.themeData,
-//               debugShowCheckedModeBanner: false,
-//               home: const Home(),
-//             );
-//           }
-
-//           return Container();
-//         },
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:them_by_bloc_flutter/bloc/theme_bloc.dart';
